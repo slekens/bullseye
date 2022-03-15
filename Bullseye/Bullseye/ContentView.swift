1fcca5350a395820cc8b10e85b4b9081ac180a8b
Abraham Abreu <slek_2001@hotmail.com>
2022-03-14 18:00:42 -0600

    Adding the main basic view

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1")
+                    .bold()
+                    .font(.body)
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+                    .font(.body)
+            }
+            Button(action: {}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            //.previewLayout(.fixed(width: 568, height: 320))
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
